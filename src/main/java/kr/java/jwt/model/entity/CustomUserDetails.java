c9037bf04068eed266dca85da8c99d0d7c90fbc4
qus0in <qus0in@gmail.com>
2025-12-21 22:32:04 +0900

    step00

---
+++ src/main/java/kr/java/jwt/model/entity/CustomUserDetails.java
@@ -0,0 +1,38 @@
+package kr.java.jwt.model.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public static CustomUserDetails from(UserAccount user) {
+        List<GrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+        );
+        return new CustomUserDetails(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
+
