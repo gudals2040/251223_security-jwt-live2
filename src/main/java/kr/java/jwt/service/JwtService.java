842ba295c3784266993c36682824427040f4fcb0
kimjava911 <aibackend4th@gmail.com>
2025-12-22 11:13:23 +0900

    step01-3

--- src/main/java/kr/java/jwt/service/JwtService.java
+++ src/main/java/kr/java/jwt/service/JwtService.java
@@ -1,5 +1,7 @@
 package kr.java.jwt.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.UUID;
 
 // 1-3
 @Service
@@ -24,4 +28,34 @@ public class JwtService {
         this.accessTokenExpiry = accessTokenExpiry; // long <- string
         this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
+
+    // 토큰 생성
+    public String generateAccessToken(Long userId, String email, String role) {
+        // import java.util.Date;
+        Date now = new Date();
+        return Jwts.builder()
+                .subject(String.valueOf(userId)) // 토큰 주인에 대한 정보
+                // 토큰에 포함하고 싶은 정보 claim
+                .claim("email", email)
+                .claim("role", role)
+                // 발행 기준
+                .issuedAt(now) // 발행된 시간
+                .expiration(new Date(now.getTime() + accessTokenExpiry)) // 15분 후 만료되는 시간
+                .id(UUID.randomUUID().toString()) // 식별
+                .signWith(secretKey) // 변환
+                .compact(); // 토큰화
+    }
+
+    // 이 토큰의 값은 무엇인가? (해석, 파싱)
+    public Claims parseToken(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey) // secretKey 기준으로 해석할 수 없을 시 에러가 발생
+                // 1) 형식을 지키지 않은 토큰 (잘못된 코드)
+                // 2) 만료된 토큰
+                .build()
+                .parseSignedClaims(token) // 토큰을 파서에 집어넣어서
+                .getPayload(); // Claims(Payload)를 추출
+    }
+
+    // 이 토큰이 제대로된 형태의 토큰인가? (검증)
 }
